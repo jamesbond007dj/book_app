6039d0c38c8c23fa617bb395daa806fd8e536a6e
bond <ilkerzaimoglu@gmail.com>
2019-10-30 11:19:42 -0700

    lab12 feature 1 done

--- server.js
+++ server.js
@@ -11,25 +11,59 @@ require('ejs');
 const app = express();
 
 const PORT = process.env.PORT || 3002;
-// const pg = require('pg');
-// const client = new pg.Client(process.env.DATABASE_URL);
-// client.on('error', err => { throw err; });
-
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', err => { throw err; });
+client.connect();
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', newSearch);
+app.get('/searches', newSearch);
 app.post('/searches', searchForBooks);
-
-function newSearch(request, response) {
-  response.render('pages/index');
-}
+app.get('/', getBooks);
+// app.post('/pages', getBooks);
 
 app.get('*', (request, response) => {
   response.status(404).send('this route does not exist');
 })
+
+function getBooks(request, response){
+  // go to the database and get all the tasks and display them
+  
+  const sql = `SELECT * FROM books;`;
+
+  client.query(sql)
+    .then(sqlResults => {
+      
+      const arrayOfBooks = sqlResults.rows;
+      console.log(arrayOfBooks);
+      response.render('pages/index', {books: arrayOfBooks});
+    })
+    .catch(error => errorHandler(error, response));
+}
+
+// function getOneTask(request, response){
+//   // go to the database, get a specific task using an id and show details of that task
+//   console.log(request.params.book_id);
+
+//   const sql = `SELECT * FROM books WHERE id=$1;`;
+//   const safeValues = [request.params.book_id];
+
+//   client.query(sql, safeValues)
+//     .then(sqlResults => {
+//       const selectedBook = sqlResults.rows[0];
+//       response.render('pages/details', {taskInfo:selectedBook})
+//     })
+//     .catch(err => {console.error(err)});
+// }
+
+
+function newSearch(request, response) {
+  response.render('pages/searches/new');
+}
+
 
 function searchForBooks(request, response) {
   console.log(request.body.search);
